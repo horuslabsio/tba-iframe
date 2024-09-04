9b906c82c60c26479cb66baa7d4b89b74cf2cae4
Catherine Jonathan <kateberryd@gmail.com>
2024-09-04 11:01:11 +0100

    feat: tokenbound iframe design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        customGray: "hsla(0, 0%, 85%, .4)",
+        primary: "#0C0C4F",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        banner: "url('/logo.avif')",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }: any) {
+      const newUtilities = {
+        ".no-scrollbar": {
+          "scrollbar-width": "none",
+        },
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
 export default config;
